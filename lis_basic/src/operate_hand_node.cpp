478f69637bacc94514a299658f7e89833f3d319a
Naohiro Hayashi <N.Hayashi1134@gmail.com>
2015-07-27 17:19:33 +0900

    test

--- lis_basic/src/operate_hand_node.cpp
+++ lis_basic/src/operate_hand_node.cpp
@@ -13,8 +13,9 @@
 
 int main(int argc, char **argv)
 {
-    ros::NodeHandle n;
     ros::init(argc, argv, "operate_hand_node");
+    ros::NodeHandle n;
+
     std::cout << "Initializing node... " << std::endl;
     ros::Publisher pub_hook = n.advertise<std_msgs::String>("hook", 10);
     std_msgs::String pub_msg;
